522411c3fa850bb3fe04e1bf6ea9fcf8949d105e
fernandofilipuzzi.utn@gmail.com <fernandofilipuzzi.ng@gmail.com>
2025-10-22 14:20:57 -0300

    prueba conectividad

---
+++ Ejemplos_MAUI_Devices/Ejemplo_Red/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ejemplo_Red;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Ejemplos_MAUI_Devices/Ejemplo_Red/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace Ejemplo_Red;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        // detectar al cargar
+        VerificarConexion();
+
+        // detectar cuando cambia el estado de red
+        Connectivity.ConnectivityChanged += (s, e) =>
+        {
+            VerificarConexion();
+        };
+    }
+
+    private void VerificarConexion()
+    {
+        var acceso = Connectivity.Current.NetworkAccess;
+        var perfiles = Connectivity.Current.ConnectionProfiles;
+
+        if (acceso != NetworkAccess.Internet)
+        {
+            DisplayAlert("Sin conexión", "No hay acceso a Internet", "OK");
+        }
+        else if (perfiles.Contains(ConnectionProfile.WiFi))
+        {
+            DisplayAlert("Conectado", "Estás en Wi-Fi", "OK");
+        }
+        else if (perfiles.Contains(ConnectionProfile.Cellular))
+        {
+            DisplayAlert("Conectado", "Estás en datos móviles", "OK");
+        }
+        else
+        {
+            DisplayAlert("Conectado", "Tipo de conexión desconocido", "OK");
+        }
+    }
+}
